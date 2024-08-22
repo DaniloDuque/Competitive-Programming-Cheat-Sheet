0b3c8a3e96af3d869a3f83d4cee8173e87b4670d
Danilo Duque <duque@mcmini.local>
2024-08-21 22:14:45 -0600

    ...

--- DataStructures/Segment_Tree.cpp
+++ DataStructures/Segment_Tree.cpp
@@ -5,25 +5,24 @@ struct STree {
     vector<int> st;int n;
     STree(int n): st(4*n+5,NEUT), n(n) {}
     void init(int k, int s, int e, int *a){
-	if(s+1==e){st[k]=a[s];return;}
-	int m=(s+e)>>1;
-	init(2*k,s,m,a);init(2*k+1,m,e,a);
-	st[k]=oper(st[2*k],st[2*k+1]);
+	    if(s+1==e){st[k]=a[s];return;}
+	    int m=(s+e)>>1;
+	    init(2*k,s,m,a);init(2*k+1,m,e,a);
+	    st[k]=oper(st[2*k],st[2*k+1]);
     }
 
     void upd(int k, int s, int e, int p, int v){
-	if(s+1==e){st[k]=v;return;}
-	int m=(s+e)>>1;
-	if(p<m)upd(2*k,s,m,p,v);
-	else upd(2*k+1,m,e,p,v);
-	st[k]=oper(st[2*k],st[2*k+1]);
+	    if(s+1==e){st[k]=v;return;}
+	    int m=(s+e)>>1;
+	    (p<m)? upd(2*k,s,m,p,v) : upd(2*k+1,m,e,p,v);
+	    st[k]=oper(st[2*k],st[2*k+1]);
     }
 
     int query(int k, int s, int e, int a, int b){
-	if(s>=b||e<=a)return NEUT;
-	if(s>=a&&e<=b)return st[k];
-	int m=(s+e)>>1;
-	return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
+	    if(s>=b||e<=a)return NEUT;
+	    if(s>=a&&e<=b)return st[k];
+	    int m=(s+e)>>1;
+	    return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
     }
 
     void init(int *a){init(1,0,n,a);}
