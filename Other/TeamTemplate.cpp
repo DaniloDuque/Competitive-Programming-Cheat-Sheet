062aaa67792877f86cc1a3757e6494659f0b9f83
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-09-02 19:32:39 -0600

    Update and rename teamTemplate.cpp to TeamTemplate.cpp

--- Other/TeamTemplate.cpp
+++ Other/TeamTemplate.cpp
@@ -1,43 +1,33 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-template <typename Key, typename Value>
-using ordered_map = tree<Key, Value, less<Key>, rb_tree_tag, tree_order_statistics_node_update>;
+using namespace __gnu_pbds;
+
+template <typename T>
+using vec=vector<T>;
 template <typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename Key, typename Value = null_type>
-using trie = trie<Key, Value, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update>;
 #define SET(m, i) ((m) | (1 << (i)))
 #define TEST(m, i) ((m) & (1 << (i)))
 #define CLEAR(m, i) ((m) &~ (1 << (i)))
-#define DEBUG(n) (cout << (n) << endl)
+#define DEBUG(n) cout<<#n<<" = "<<n<<'\n'
 #define CLEAN(arr) (memset(arr, 0, sizeof(arr)))
 #define ALL(v) (v).begin(), (v).end()
-
-
-typedef long long int ll;
-typedef std::vector<int> vec;
-using namespace std;
+#define ll long long
 
 const int MAX = 2e5+20;
+int t = 1;
 
-void sol(){        
+void solve(){        
     
 }
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
-              
-    int t;
-    cin >> t;
+    cin>>t;
     while(t--){
-        sol();
-    }
-
-
-    return 0;
+        solve();
+    }return 0;
 }
