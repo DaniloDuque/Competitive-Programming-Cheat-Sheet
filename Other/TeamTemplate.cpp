65c182c90b0b5a2422476fd23d79e1ad9d871db9
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-10-07 15:13:17 -0600

    Update TeamTemplate.cpp

--- Other/TeamTemplate.cpp
+++ Other/TeamTemplate.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-using vec=vector<T>;
 #define SET(m, i) ((m) | (1ULL << (i)))
 #define TEST(m, i) ((m) & (1ULL << (i)))
 #define CLEAR(m, i) ((m) &~ (1ULL << (i)))
 #define DEBUG(n) cout<<#n<<" = "<<n<<'\n'
 #define CLEAN(arr) (memset(arr, 0, sizeof(arr)))
 #define ALL(v) (v).begin(), (v).end()
+#define vec vector
 #define snd second
 #define fst first
 #define ll long long
 const int MAX = 2e5+20;
-int t = 1;
+int t=1;
 
 void solve(){        
     
