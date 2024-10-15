1e25a1c97c57c046ecf87e6292ab314d8688341c
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-10-15 16:21:26 -0600

    Update FermatsLittleTheorem.cpp

--- Math/FermatsLittleTheorem.cpp
+++ Math/FermatsLittleTheorem.cpp
@@ -1,7 +1,7 @@
 int exp(int b, int e){
     if(!e) return 1;
-    int r = exp((b*b)%MOD, e>>1);
-    return ((b*(e&1))*r)%MOD;
+    if(e&1) return (b*exp((b*b)%MOD, e>>1))%MOD;
+    return exp((b*b)%MOD, e>>1);
 }
 
 int inverse(int n){return exp(n, MOD-2);}
