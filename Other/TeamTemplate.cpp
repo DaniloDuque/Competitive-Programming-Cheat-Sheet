4e877a4c51058085ed3ee897916529f337ed265e
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2025-05-06 21:00:44 -0600

    Update TeamTemplate.cpp

--- Other/TeamTemplate.cpp
+++ Other/TeamTemplate.cpp
@@ -4,27 +4,30 @@ using namespace std;
 #define SET(m, i) ((m) | (1ULL << (i)))
 #define TEST(m, i) ((m) & (1ULL << (i)))
 #define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define slice(a, n) ranges::subrange(a, a+n)
 #define DEBUG(n) cout<<#n<<" = "<<n<<endl
-#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define DEBUG_ALL(a) for(auto &ee:a) cout<<ee<<' ';cout<<endl;
 #define ALL(v) (v).begin(), (v).end()
+#define ALLR(a) (a), (a+n)
 #define vec vector
 #define snd second
 #define fst first
 #define pb push_back
 #define ll long long
-const int MAX = 2e5+20, MOD = 1e9+7;
+const ll MAX = 2e5+20, MOD = 1e9+7;
 ll n;
 
 void solve(){        
-    
+    cin>>n;
 }
 
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int t=1;
-    cin>>t;
-    while(t--){
+    int T=1;
+    cin>>T;
+    while(T--){
         solve();
+        //cout<<(solve()? "YES" : "NO")<<endl;
     }return 0;
 }
